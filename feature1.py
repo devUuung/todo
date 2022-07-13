eb805f0201dd8ea8f3062ac16102e8dfda4a2d70
GNU_CS_UngSik <87903147+blpeng2@users.noreply.github.com>
2022-07-13 13:18:13 +0000

    notion databse create 기능

--- feature1.py
+++ feature1.py
@@ -1,15 +1,38 @@
 import requests
+from dotenv import load_dotenv
+import os
 
-url = "https://api.notion.com/v1/databases/cbaba5e4327b46a38d7af82abf7d29ab"
+load_dotenv()
 
-payload = {"properties": "이름"}
+database_id = os.environ.get("NOTION_DATABASE")
+notion_api = os.environ.get("NOTION_API")
+
+url = "https://api.notion.com/v1/pages"
+
+payload = {
+    "parent": {
+        "type": "database_id",
+        "database_id": f"{database_id}"
+    },
+    "properties": {
+        "Name": {
+            "title":
+            [
+                {
+                    "type": "text",
+                    "text": {"content": "The title"}
+                }
+            ]
+        }
+    }
+}
 headers = {
     "Accept": "application/json",
     "Notion-Version": "2022-06-28",
     "Content-Type": "application/json",
-    "Authorization": "Bearer secret_6tcCZwI8UChaAKW9BGYY33ZBQPZg2aoFlGxxj4VuEGI"
+    "Authorization": f"{notion_api}"
 }
 
-response = requests.patch(url, json=payload, headers=headers)
+response = requests.post(url, json=payload, headers=headers)
 
 print(response.text)
